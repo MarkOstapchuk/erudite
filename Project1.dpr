a3a1098837012504d3eba76052fb9f16e626b902
Mark <markostapchukwork@gmail.com>
2022-12-14 14:40:12 +0300

    creating project

---
+++ Project1.dpr
@@ -0,0 +1,16 @@
+﻿program Project1;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  Windows;
+
+begin
+  setConsoleCP(1251);
+  setConsoleOutputCP(1251);
+  writeln('аллв');
+  readln;
+end.
